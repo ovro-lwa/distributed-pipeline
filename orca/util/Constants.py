ee88db917c0e026380248c106cbabd2f51787849
Yuping Huang <yupinghyper@gmail.com>
2019-02-27 15:52:02 -0800

    Add Constants.py;sync script with astm and fetch the notebooks.

---
+++ orca/util/Constants.py
@@ -0,0 +1,6 @@
+from astropy import units as u
+
+# From Google Map cutout on http://www.tauceti.caltech.edu/LWA/
+LWA_LAT = 37.239883 * u.deg
+LWA_LON = -118.281675 * u.deg
+LWA_ALTITUDE = 124 * u.m we probably don't need this and also I am actually not sure
---
+++ sync_with_astm.sh
@@ -0,0 +1,8 @@
+# Sync code to astm and notebooks from astm.
+REMOTE_DIR=/home/yuping/transient/
+LOCAL_DIR=/home/dynamic/astm-workspace/astm-transient-stuff/
+REMOTE_HOST=astmhead
+REMOTE_USER=yuping
+
+rsync -aP --delete --exclude 'notebooks' $LOCAL_DIR ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR} 
+rsync -aP ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}/notebooks $LOCAL_DIR/
