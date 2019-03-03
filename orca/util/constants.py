86af65bd8d1902ef03fb77f28504c5e27aa99875
Yuping Huang <yupinghyper@gmail.com>
2019-03-02 16:03:53 -0800

    added more constants.

--- orca/util/constants.py
+++ orca/util/constants.py
@@ -3,4 +3,9 @@ from astropy import units as u
 # From Google Map cutout on http://www.tauceti.caltech.edu/LWA/
 LWA_LAT = 37.239883 * u.deg
 LWA_LON = -118.281675 * u.deg
-LWA_ALTITUDE = 124 * u.m we probably don't need this and also I am actually not sure
+LWA_ALTITUDE = 124 * u.m
+
+# Could also write a method to figure these out from an ms post-expansion
+LWA_N_CHAN = 109
+LWA_N_SPW = 22
+LWA_N_ANT = 256
