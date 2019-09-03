b9a7a67f4123ce3ec7e0e0ed90a98292c7cde9e6
Yuping Huang <yupinghyper@gmail.com>
2019-09-03 10:46:08 -0700

    Sidereal images + psf made.

--- orca/proj/longbandpass.py
+++ orca/proj/longbandpass.py
@@ -1,13 +1,14 @@
 from .celery import app
 from celery import group, chain
-from ..transform import dada2ms, change_phase_centre, averagems, peeling, imaging
+from ..transform import dada2ms, change_phase_centre, averagems, peeling, imaging, sidereal_subtraction_kit
 from ..flagging import flag_bad_chans, merge_flags
 from ..utils import image_sub
-from datetime import datetime
+from datetime import datetime, timedelta
 import os, sys
 import logging
 import glob
 import shutil
+from typing import List, Tuple
 import uuid
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
@@ -98,9 +99,36 @@ def subsequent_frame_subtraction(dir1, dir2, datetime_1, datetime_2, out_dir):
         shutil.rmtree(temp_tree)
 
 
+# OH MAN I NEED TO REFACTOR OUT THIS ABOMNIATION OF DUPLICATE CODE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 @app.task
-def sidereal_subtraction():
-    # merge flag, subtract
+def prep_image_for_sidereal_subtraction(dir1, dir2, datetime_1, datetime_2, out_dir1, out_dir2):
+    # Copy over to local disk, chgcentre, merge flag, and then subtract, write back.
+    temp_tree = f'/pipedata/workdir/yuping/{uuid.uuid4()}'
+    # I'd rather have an error thrown here if there's a UUID clash which shouldn't ever happen.
+    os.mkdir(temp_tree)
+    try:
+        tree1 = f'{temp_tree}/{datetime_1}'
+        tree2 = f'{temp_tree}/{datetime_2}'
+        shutil.copytree(f'{dir1}/{datetime_1}', tree1)
+        shutil.copytree(f'{dir2}/{datetime_2}', tree2)
+        new_phase_center = change_phase_centre.get_phase_center(f'{tree1}/00_{datetime_1}.ms')
+        spws = [f'{i:02d}' for i in range(22)]
+        for s in spws:
+            merge_flags.merge_flags(f'{tree1}/{s}_{datetime_1}.ms', f'{tree2}/{s}_{datetime_2}.ms')
+            change_phase_centre.change_phase_center(f'{tree2}/{s}_{datetime_2}.ms', new_phase_center)
+        im1, psf = imaging.make_image(sorted(glob.glob(f'{tree1}/??_{datetime_1}.ms')), datetime_1,
+                                      temp_tree, make_psf=True)
+        im2 = imaging.make_image(sorted(glob.glob(f'{tree2}/??_{datetime_2}.ms')), datetime_2, temp_tree)
+        shutil.copy(im1, out_dir1)
+        shutil.copy(psf, out_dir1)
+        shutil.copy(im2, out_dir2)
+    finally:
+        shutil.rmtree(temp_tree)
+
+
+@app.task
+def sidereal_subtract_image(im1_path, im2_path, out_dir):
+    sidereal_subtraction_kit.subtract_images(im1_path, im2_path, out_dir)
     pass
 
 @app.task
@@ -173,3 +201,34 @@ def do_subsequent_frame_subtraction():
                                          os.path.basename(ms_list[i]), os.path.basename(ms_list[i+1]),
                                  '/lustre/yuping/0-100-hr-reduction/qual/subsequent-subtraction/2018-03-23/hh=02') for
           i, dir1 in enumerate(ms_list[:-1]))()
+
+
+def generate_ms_pairs() -> List[Tuple[datetime, datetime]]:
+    sday = timedelta(days=0, hours=23, minutes=56, seconds=4)
+    day_1_times = [ datetime.fromisoformat(os.path.basename(p)) for p in
+                    sorted(glob.glob('/lustre/yuping/0-100-hr-reduction/qual/msfiles/2018-03-22/hh=02/*'))]
+    return [(one, one + sday) for one in day_1_times if one > datetime(2018, 3, 22, 2, 3, 56)]
+
+
+def prep_sidereal_subtraction():
+    """TODO for sidereal subtraction involving more than one day. This directory structure won't really work since
+    the flags will be different.
+    """
+    ms_pairs = generate_ms_pairs()
+    logging.info(f'There are {len(ms_pairs)} pairs of sidereally separated measurement sets to process.')
+    group(prep_image_for_sidereal_subtraction.s(
+        '/lustre/yuping/0-100-hr-reduction/qual/msfiles/2018-03-22/hh=02',
+        '/lustre/yuping/0-100-hr-reduction/qual/msfiles/2018-03-23/hh=02',
+        p[0].isoformat(), p[1].isoformat(),
+        '/lustre/yuping/0-100-hr-reduction/qual/prep-sidereal-images/2018-03-22/hh=02',
+        '/lustre/yuping/0-100-hr-reduction/qual/prep-sidereal-images/2018-03-23/hh=02')
+          for p in ms_pairs)()
+
+
+def do_sidereal_subtraction():
+    # call the kit
+    # do the subtraction
+    out_dir = '/lustre/yuping/0-100-hr-reduction/qual/sidereal-subtraction-1/2018-03-22/hh=02'
+    pass
+
+
--- orca/transform/imaging.py
+++ orca/transform/imaging.py
@@ -10,11 +10,19 @@ WSCLEAN_1_11_EXEC = '/opt/astro/wsclean-1.11-gcc4.8.5_cxx11/bin/wsclean'
 """
 
 
-def make_image(ms_list: List[str], date_times_string: str, out_dir: str):
+def make_image(ms_list: List[str], date_times_string: str, out_dir: str, make_psf: bool=False):
     subprocess.check_output([WSCLEAN_1_11_EXEC, '-size', '4096', '4096', '-scale', '0.03125',
                              '-niter', '0', '-weight', 'briggs', '0',
                              '-weighting-rank-filter', '3', '-weighting-rank-filter-size', '128',
                              '-no-update-model-required', '-no-reorder',
                              '-j', '10', '-name', f'{out_dir}/{date_times_string}'] + ms_list)
-    return f'{out_dir}/{date_times_string}-image.fits'
+    if make_psf:
+        subprocess.check_output([WSCLEAN_1_11_EXEC, '-size', '8192', '8192', '-scale', '0.03125',
+                                 '-niter', '0', '-weight', 'briggs', '0',
+                                 '-weighting-rank-filter', '3', '-weighting-rank-filter-size', '128',
+                                 '-no-update-model-required', '-no-reorder', '-make-psf-only',
+                                 '-j', '10', '-name', f'{out_dir}/{date_times_string}'] + ms_list)
+        return f'{out_dir}/{date_times_string}-image.fits', f'{out_dir}/{date_times_string}-psf.fits'
+    else:
+        return f'{out_dir}/{date_times_string}-image.fits'
 
