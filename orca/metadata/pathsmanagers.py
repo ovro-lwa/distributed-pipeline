05ba1f01d5cc843901cb807f9418009d060998f0
Yuping Huang <yupinghyper@gmail.com>
2019-10-28 17:44:30 -0700

    DefaultPathsManager implemented.

--- orca/metadata/pathsmanagers.py
+++ orca/metadata/pathsmanagers.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
-
+from os import path
 
 class PathsManager(ABC):
     def __init__(self, utc_times_txt_path: str):
@@ -41,20 +41,24 @@ class OfflinePathsManager(PathsManager):
 
     This could potentially work for processing the buffer too. A config file reader will probably parse a config
     file into this object.
+
+    Assumes that the bandpass calibration table is named like bcal_dir/00.bcal'
     """
     def __init__(self, utc_times_txt_path: str, msfile_dir: str, bcal_dir: str, flag_npy_path: str):
+        for d in (msfile_dir, bcal_dir, flag_npy_path):
+            if not path.exists(d):
+                raise FileNotFoundError(f"File not found or path does not exist: {d}.")
         super().__init__(utc_times_txt_path)
-        # Validation?
         self.msfile_dir = msfile_dir
         self.bcal_dir = bcal_dir
         self.flag_npy_path = flag_npy_path
 
     def get_gaintable_path(self,  spw: str):
-        return f'{self.bcal_dir}/{spw}'
+        return f'{self.bcal_dir}/{spw}.bcal'
 
     def get_ms_path(self, timestamp: datetime, spw: str):
         """
-        Return a path that looks like /path/to/msfile/2018-03-02/hh=02/2018-03-02T02:02:02/00_2018-03-02T02:02:02.ms
+        ms path should looks like /path/to/msfile/2018-03-02/hh=02/2018-03-02T02:02:02/00_2018-03-02T02:02:02.ms
         :param timestamp:
         :param spw:
         :return:
@@ -64,4 +68,9 @@ class OfflinePathsManager(PathsManager):
         return f'{self.msfile_dir}/{date}/{hour}/{timestamp.isoformat()}/{spw}_{timestamp.isoformat()}.ms'
 
     def get_flag_npy_path(self, timestamp):
+        """
+        Returns the same flag npy file regardless of the timestamp...
+        :param timestamp:
+        :return:
+        """
         return self.flag_npy_path
--- tests/metadata/test_pathsmanagers.py
+++ tests/metadata/test_pathsmanagers.py
@@ -6,10 +6,19 @@ from datetime import datetime
 
 def test_read_utc_times():
     pm = OfflinePathsManager(f'{path.dirname(__file__)}/../resources/utc_times_test.txt',
-                             None, None, None)
+                             '.', '.', '.')
     assert len(pm._mapping) == 10
     assert pm._mapping[datetime(2019, 10, 28, 23, 4, 44)] == \
            '2019-10-22-02:35:47_0005246685093888.000000.dada'
 
+
+def test_with_bad_path():
+    with pytest.raises(FileNotFoundError):
+        pm = OfflinePathsManager(f'{path.dirname(__file__)}/../resources/utc_times_test.txt',
+                                 '/some/bad/path', '.', '.')
+
+
 def test_get_gaintable_path():
-    pass
+    pm = OfflinePathsManager(f'{path.dirname(__file__)}/../resources/utc_times_test.txt',
+                             '.', '.', '.')
+    assert pm.get_gaintable_path('00') == './00.bcal'
