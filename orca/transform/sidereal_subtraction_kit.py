1d4e965ee1cea8b5884f74933613cbdbbfb719b8
Yuping Huang <yupinghyper@gmail.com>
2019-11-12 11:55:10 -0800

    Bug fix for Crab subtraction: transpose the PSF along with the image.

--- orca/transform/sidereal_subtraction_kit.py
+++ orca/transform/sidereal_subtraction_kit.py
@@ -3,6 +3,8 @@ import logging
 import os
 import numpy as np
 
+log = logging.getLogger(__name__)
+
 
 def subtract_images(im1_path: str, im2_path: str, out_dir: str, psf_path: str=None, subtract_crab: bool=False,
                     shift: bool=False, scale: bool=False):
@@ -11,7 +13,7 @@ def subtract_images(im1_path: str, im2_path: str, out_dir: str, psf_path: str=No
     im2, _ = fitsutils.read_image_fits(im2_path)
     if subtract_crab:
         #find the peak, subtract 0.85 of it; then find an adjacent peak, do like 0.4 percent
-        logging.warn('The crab box is hardcoded.')
+        log.warning('The crab box is hardcoded.')
         psf, _ = fitsutils.read_image_fits(psf_path)
         im1 = sub_crab(im1, psf)
         im2 = sub_crab(im2, psf)
@@ -27,23 +29,21 @@ def subtract_images(im1_path: str, im2_path: str, out_dir: str, psf_path: str=No
 
 def sub_crab(im, psf):
     im_T = im.T
-    tauA = im_T[2190:2690, 1438:1808]
-    peakx, peaky = np.unravel_index(np.argmax(tauA), tauA.shape)
-    peakx += 2190
-    peaky += 1438
-    logging.info(f'peakx is {peakx} and peaky is {peaky}.')
-    sub1 = sub_source(im_T, psf, 0.85, peakx, peaky)
+    # tauA = im_T[2190:2690, 1438:1808]
+    tauA = im_T
+    init_peakx, init_peaky = np.unravel_index(np.argmax(tauA), tauA.shape)
+    # init_peakx += 2190
+    # init_peaky += 1438
     # Find the brightest pixel that's right next to the original peak and subtract
-    displacements = [(-1, -1), (-1, 0), (-1, 1), (0, 0), (0, -1), (0, 1), (1, 1)]
-    displacement = displacements[np.argmax([sub1[peakx + i, peaky + j] for i, j in displacements])]
-    peakx += displacement[0]
-    peaky += displacement[1]
-    if sub1[peakx, peaky] > 50:
-        sub2 = sub_source(sub1, psf, 0.5, peakx, peaky)
-    else:
-        logging.info(f'Not subtracting the second peak. with flux {sub1[peakx, peaky]}.')
-        sub2=sub1
-    return sub2.T
+    square_around_crab = im_T[init_peakx-1:init_peakx+2, init_peaky-1: init_peaky+2]
+    peakx, peaky = 0, 0
+    sub = im_T
+    while True:
+        sub = sub_source(sub, psf.T, 0.1, peakx + init_peakx - 1, peaky + init_peaky - 1)
+        peakx, peaky = np.unravel_index(np.argmax(square_around_crab), square_around_crab.shape)
+        if square_around_crab[peakx, peaky] < 10:
+            break
+    return sub.T
 
 
 def sub_source(im, psf, gain, peakx, peaky):
@@ -51,9 +51,8 @@ def sub_source(im, psf, gain, peakx, peaky):
     psf_size = psf.shape[0]
     assert psf_size >= 2*im_size
     peakval = im[peakx, peaky]
-    # Take a chunk centered at the peak, do the subtraction, write it back to the image
+    # Take a chunk centered at the peak, do the subtraction
     trunc_psf = psf[psf_size//2 - peakx: psf_size//2 + im_size - peakx,
                    psf_size//2 - peaky : psf_size//2 + im_size - peaky]
-    out_image = np.array(im)
-    out_image -= gain * peakval * trunc_psf
-    return out_image
+    im -= gain * peakval * trunc_psf
+    return im
