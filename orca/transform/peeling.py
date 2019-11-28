a86153e1268a929158ad7160efb480252785a8f3
Yuping Huang <yupinghyper@gmail.com>
2019-11-28 07:16:14 -0800

    Added env variables for peeling.

--- orca/transform/peeling.py
+++ orca/transform/peeling.py
@@ -1,5 +1,7 @@
 import subprocess
-import  logging
+import logging
+import os
+
 log = logging.getLogger(__name__)
 
 TTCAL_EXEC = '/opt/astro/mwe/bin/ttcal-0.3.0'
@@ -13,9 +15,14 @@ def peel_with_ttcal(ms: str, sources: str):
     :param sources:
     :return:
     """
-    try:
-        subprocess.check_call([TTCAL_EXEC, 'peel', ms, sources, '--beam', 'sine', '--maxiter', '50',
-                               '--tolerance', '1e-4', '--minuvw', '10'], stderr=subprocess.STDOUT)
-        return ms
-    except subprocess.CalledProcessError as e:
-        log.error(e)
+    new_env = dict(os.environ, LD_LIBRARY_PATH='/opt/astro/mwe/usr/lib64:/opt/astro/lib/',
+                   AIPSPATH='/opt/astro/casa-data dummy dummy')
+    proc = subprocess.Popen([TTCAL_EXEC, 'peel', ms, sources, '--beam', 'sine', '--maxiter', '50',
+                             '--tolerance', '1e-4', '--minuvw', '10'], env=new_env,
+                            stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    stdoutdata, stderrdata = proc.communicate()
+    if proc.returncode is not 0:
+        logging.error(f'Error in TTCal: {stderrdata}')
+        logging.info(f'stdout is {stdoutdata}')
+        raise Exception('Error in TTCal.')
+    return ms
