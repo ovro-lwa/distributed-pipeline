3d72ab8840f3e564ef3acb79f0fd21dab90a7bd8
Yuping Huang <yupinghyper@gmail.com>
2019-12-02 12:25:18 -0800

    outlier notebook; bug fixes.

--- orca/transform/change_phase_centre.py
+++ orca/transform/change_phase_centre.py
@@ -1,14 +1,19 @@
 import subprocess
 from casacore.tables import table
 from astropy.coordinates import SkyCoord
+import os
+
 
 CHGCENTRE_PATH = '/opt/astro/wsclean/bin/chgcentre'
 
 
 def change_phase_center(ms: str, center_dir: str) -> None:
+    new_env = dict(os.environ, LD_LIBRARY_PATH='/opt/astro/mwe/usr/lib64:/opt/astro/lib/:/opt/astro/casacore-1.7.0/lib',
+                   AIPSPATH='/opt/astro/casa-data dummy dummy')
     ra, dec = center_dir.split(' ')
     try:
-        subprocess.check_output([CHGCENTRE_PATH, ms, ra, dec], stderr=subprocess.STDOUT)
+        p = subprocess.Popen([CHGCENTRE_PATH, ms, ra, dec], env=new_env)
+        p.communicate()
     except subprocess.CalledProcessError as e:
         raise e
 
--- orca/transform/imaging.py
+++ orca/transform/imaging.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 from typing import List
 
 WSCLEAN_1_11_EXEC = '/opt/astro/wsclean-1.11-gcc4.8.5_cxx11/bin/wsclean'
@@ -10,17 +11,21 @@ WSCLEAN_1_11_EXEC = '/opt/astro/wsclean-1.11-gcc4.8.5_cxx11/bin/wsclean'
 
 
 def make_image(ms_list: List[str], date_times_string: str, out_dir: str, make_psf: bool=False):
-    subprocess.check_output([WSCLEAN_1_11_EXEC, '-size', '4096', '4096', '-scale', '0.03125',
-                             '-niter', '0', '-weight', 'briggs', '0',
-                             '-weighting-rank-filter', '3', '-weighting-rank-filter-size', '128',
-                             '-no-update-model-required', '-no-reorder',
-                             '-j', '10', '-name', f'{out_dir}/{date_times_string}'] + ms_list)
+    new_env = dict(os.environ, LD_LIBRARY_PATH='/opt/astro/mwe/usr/lib64:/opt/astro/lib/:/opt/astro/casacore-1.7.0/lib',
+                   AIPSPATH='/opt/astro/casa-data dummy dummy')
+    p = subprocess.Popen([WSCLEAN_1_11_EXEC, '-size', '4096', '4096', '-scale', '0.03125',
+                          '-niter', '0', '-weight', 'briggs', '0',
+                          '-weighting-rank-filter', '3', '-weighting-rank-filter-size', '128',
+                          '-no-update-model-required', '-no-reorder',
+                          '-j', '10', '-name', f'{out_dir}/{date_times_string}'] + ms_list, env=new_env)
+    p.communicate()
     if make_psf:
-        subprocess.check_output([WSCLEAN_1_11_EXEC, '-size', '8192', '8192', '-scale', '0.03125',
-                                 '-niter', '0', '-weight', 'briggs', '0',
-                                 '-weighting-rank-filter', '3', '-weighting-rank-filter-size', '128',
-                                 '-no-update-model-required', '-no-reorder', '-make-psf-only',
-                                 '-j', '10', '-name', f'{out_dir}/{date_times_string}'] + ms_list)
+        p = subprocess.Popen([WSCLEAN_1_11_EXEC, '-size', '8192', '8192', '-scale', '0.03125',
+                              '-niter', '0', '-weight', 'briggs', '0',
+                              '-weighting-rank-filter', '3', '-weighting-rank-filter-size', '128',
+                              '-no-update-model-required', '-no-reorder', '-make-psf-only',
+                              '-j', '10', '-name', f'{out_dir}/{date_times_string}'] + ms_list, env=new_env)
+        p.communicate()
         return f'{out_dir}/{date_times_string}-image.fits', f'{out_dir}/{date_times_string}-psf.fits'
     else:
         return f'{out_dir}/{date_times_string}-image.fits'
