808ef10cc66a42c4fe240b11fb87bcd70cb08585
Yuping Huang <yupinghyper@gmail.com>
2020-02-11 08:53:32 -0800

    Sidereal subtraction kit update.

--- orca/transform/sidereal_subtraction_kit.py
+++ orca/transform/sidereal_subtraction_kit.py
@@ -12,36 +12,34 @@ def subtract_images(im1_path: str, im2_path: str, out_dir: str, psf_path: str=No
     im1, header = fitsutils.read_image_fits(im1_path)
     im2, _ = fitsutils.read_image_fits(im2_path)
     if subtract_crab:
-        #find the peak, subtract 0.85 of it; then find an adjacent peak, do like 0.4 percent
         log.warning('The crab box is hardcoded.')
         psf, _ = fitsutils.read_image_fits(psf_path)
         im1 = sub_crab(im1, psf)
         im2 = sub_crab(im2, psf)
     if scale:
         # just minimize the stdev in the inner 1000 pixel by brute-forcing
-        scales = np.linspace(0.90, 1.10, num=81)
-        ind = np.argmin([np.std(
-            x * im1[2048 - 500:2048 + 500, 2048 - 500:2048 + 500] - im2[2048 - 500:2048 + 500, 2048 - 500:2048 + 500])
-            for x in scales])
+        scales = np.linspace(0.90, 1.10, num=101)
+        ind = np.argmin(np.std(
+            np.outer(scales, im1[2048 - 500:2048 + 500, 2048 - 500:2048 + 500].flatten()) -
+            im2[2048 - 500:2048 + 500, 2048 - 500:2048 + 500].flatten(), axis=1))
         im1 = im1 * scales[ind]
     fitsutils.write_image_fits(f'{out_dir}/diff_{os.path.basename(im1_path)}', header, im2 - im1, overwrite=True)
 
 
 def sub_crab(im, psf):
     im_T = im.T
-    # tauA = im_T[2190:2690, 1438:1808]
-    tauA = im_T
+    tauA = im_T[2190:2690, 1438:1808]
     init_peakx, init_peaky = np.unravel_index(np.argmax(tauA), tauA.shape)
-    # init_peakx += 2190
-    # init_peaky += 1438
+    init_peakx += 2190
+    init_peaky += 1438
     # Find the brightest pixel that's right next to the original peak and subtract
     square_around_crab = im_T[init_peakx-1:init_peakx+2, init_peaky-1: init_peaky+2]
-    peakx, peaky = 0, 0
+    peakx, peaky = 1, 1
     sub = im_T
     while True:
-        sub = sub_source(sub, psf.T, 0.1, peakx + init_peakx - 1, peaky + init_peaky - 1)
+        sub = sub_source(sub, psf.T, 0.4, peakx + init_peakx - 1, peaky + init_peaky - 1)
         peakx, peaky = np.unravel_index(np.argmax(square_around_crab), square_around_crab.shape)
-        if square_around_crab[peakx, peaky] < 10:
+        if square_around_crab[peakx, peaky] < 40:
             break
     return sub.T
 
