09356e7141a492a3622df864e5c5ea7e6e748114
Yuping Huang <yupinghyper@gmail.com>
2020-03-23 10:02:49 -0700

    Task routing based on environmental variable.

--- orca/proj/celery.py
+++ orca/proj/celery.py
@@ -1,7 +1,10 @@
 from __future__ import absolute_import, unicode_literals
+import os
 from celery import Celery
 
+print(os.environ['HOME'])
 CELERY_APP_NAME = 'proj'
+DEVELOPMENT_VARIABLE = 'ORCA_DEV'
 
 app = Celery(CELERY_APP_NAME,
              broker='pyamqp://yuping:yuping@astm13:5672/yuping',
@@ -15,6 +18,14 @@ app.conf.update(
     result_expires=3600,
     worker_prefetch_multiplier=1
 )
+
+# TODO: maybe I should split tasks in boilerplate based on queue?
+if DEVELOPMENT_VARIABLE in os.environ:
+    task_routes = {'*': os.environ[DEVELOPMENT_VARIABLE]}
+else:
+    task_routes = {'*': 'default-1-per-node'}
+
+app.conf.task_routes = task_routes
 
 if __name__ == '__main__':
     app.start()
