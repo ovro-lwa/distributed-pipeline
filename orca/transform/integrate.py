97dd6f23592293f136b4f304dae4e93e47e4e0f3
Yuping Huang <yupinghyper@gmail.com>
2020-05-25 15:10:20 -0700

    Integrate uses phase center of the first ms by default.

--- orca/transform/integrate.py
+++ orca/transform/integrate.py
@@ -11,7 +11,7 @@ from orca.wrapper import change_phase_centre
 def integrate(ms_list: List[str], out_ms: str, phase_center: Optional[str]) -> str:
     """
     "integrate" a list of ms (assumed to be time sorted) by running chgcentre, concat, and then
-    changing the field id. Common phase center is defaulted to the phase center of the scan in the middle of the list.
+    changing the field id. Common phase center is defaulted to the phase center of the first scan in the list.
     :param ms_list:
     :param out_ms:
     :param phase_center:
@@ -20,7 +20,7 @@ def integrate(ms_list: List[str], out_ms: str, phase_center: Optional[str]) -> s
     # error out if out_ms exist.
     if os.path.exists(out_ms):
         raise FileExistsError(f"Can't concat when output ms {out_ms} already exists.")
-    phase_center = phase_center if phase_center else change_phase_centre.get_phase_center(ms_list[len(ms_list)//2])
+    phase_center = phase_center if phase_center else change_phase_centre.get_phase_center(ms_list[0])
     for ms in ms_list:
         change_phase_centre.change_phase_center(ms, phase_center)
     concat(ms_list, out_ms)
