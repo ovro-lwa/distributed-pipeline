7a097446dc88acc9a4581a3dcd3edc0400af2a0e
marinanderson <mmanders@astro.caltech.edu>
2020-07-14 17:13:56 -0700

    More type issue fixes.

--- orca/utils/calibrationutils.py
+++ orca/utils/calibrationutils.py
@@ -74,7 +74,7 @@ def gen_model_ms_stokes(ms: str, zest: bool = False):
     outbeam = beam(ms)
     
     for s,src in enumerate(cal_srcs):
-        src_position: str = src['position']
+        src_position: str = src.get('position')
         ra  = src_position.split(' ')[1]
         dec = src_position.split(' ')[2]
         if is_visible(SkyCoord(ra, dec, frame=ICRS), utctime):
@@ -86,7 +86,7 @@ def gen_model_ms_stokes(ms: str, zest: bool = False):
     
     cl = componentlist()
     if zest:
-        fluxIvals  = [src['Stokes'][0] for src in cal_srcs]
+        fluxIvals  = [src.get('Stokes')[0] for src in cal_srcs]
         sortedinds = np.argsort(fluxIvals)[::-1]
         cllist     = []
         counter    = 0
