88757ea302195fdb424c8132bbab5d65d27be158
Yuping Huang <yupinghyper@gmail.com>
2020-10-16 10:28:10 -0700

    Add onedayaverage task without keeping the ms.

--- orca/proj/onedayaverage.py
+++ orca/proj/onedayaverage.py
@@ -1,9 +1,15 @@
 from ..transform.averagems import average_ms
 from .celery import app
+from orca.wrapper import dada2ms
 import shutil
 import glob
 import os
 import logging
+import uuid
+
+from datetime import datetime
+from casacore import tables
+from typing import List
 
 log = logging.getLogger(__name__)
 
@@ -17,6 +23,26 @@ def run_average(ms_file_list, ref_ms_index, out_ms, fault_tolerant=True):
     logging.info('Finished averaging. Copying the final measurement set from /dev/shm back.')
     shutil.copytree(temp_ms, out_ms)
     shutil.rmtree(temp_ms)
+
+
+@app.task
+def running_average(dada_list: List[str], bcal: str, spw: str, out_ms: str, scratch_dir: str):
+    tmpdir = f'{scratch_dir}/{uuid.uuid4()}'
+    os.makedirs(tmpdir)
+    count = len(dada_list)
+    dada2ms.dada2ms(dada_list[0], out_ms, bcal)
+    with tables.table(out_ms) as t:
+        avg = t.getcol('DATA') / count
+
+    for i, dada in enumerate(dada_list[1:]):
+        ms = dada2ms.dada2ms(dada, f'{tmpdir}/{spw}_{i+1}.ms', bcal)
+        with tables.table(ms) as t:
+            avg += t.getcol('DATA') / count
+        shutil.rmtree(ms)
+
+    with tables.table(out_ms, readonly=False, ack=False) as out_t:
+        out_t.putcol('DATA', avg)
+    shutil.rmtree(tmpdir)
 
 
 def do_average_ms():
