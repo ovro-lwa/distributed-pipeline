1e59efd755766da3a686b32ab7ce21530b888d7f
Yuping Huang <yupinghuang@users.noreply.github.com>
2021-02-24 13:35:18 -0800

    Colorscale change.

--- orca/extra/sifting.py
+++ orca/extra/sifting.py
@@ -1,8 +1,10 @@
 from typing import List
 from datetime import datetime, timedelta
+import numpy as np
 
 import ipywidgets as widgets
 import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize
 from astropy.coordinates import SkyCoord
 import astropy.units as u
 from astropy.table import Table
@@ -14,6 +16,8 @@ from orca.utils import fitsutils, coordutils, gitutils
 from orca.metadata.pathsmanagers import OfflinePathsManager
 
 WIDTH = 128
+
+SNR_CUTOFF = 6.5
 
 
 # TODO need a back button
@@ -41,32 +45,36 @@ class SiftingWidget(widgets.HBox):
         buttons = self.make_buttons()
         self.text = widgets.HTML()
 
+        rms = np.median(np.unique(self.cat['local_rms']))
         with snapshot:
-            fig1, ax1 = plt.subplots(constrained_layout=True, figsize=(8, 8), dpi=72)
-            fig1.canvas.header_visible = False
+            self.fig1, ax1 = plt.subplots(constrained_layout=True, figsize=(8, 8), dpi=72)
+            self.fig1.canvas.header_visible = False
             ax1.set_frame_on(False)
             ax1.tick_params(labelbottom=False, labelleft=False, length=0)
-            ax1.imshow(self.diff_im, norm=Normalize(-2, 5), cmap='gray', origin='lower')
+            self.big_diff_imshow = ax1.imshow(
+                self.diff_im, norm=Normalize(-6 * rms, 6 * rms), cmap='gray', origin='lower')
 
         # TODO swap for a vbox of three plots so that I can lock the colorscale?
         with cutouts:
             self.fig2, axs2 = plt.subplots(1, 3, constrained_layout=True, figsize=(10, 3.5), dpi=72)
             self.fig2.canvas.header_visible = False
+            peak = np.abs(self.cat['peak_flux'][self.curr])
+            norm = Normalize(vmin=-peak, vmax=peak)
             for ax in axs2:
                 ax.set_frame_on(False)
                 ax.tick_params(labelbottom=False, labelleft=False, length=0)
             self.diff_imshow = axs2[0].imshow(
                 self.diff_im.T[self.cat['x'][self.curr] - WIDTH: self.cat['x'][self.curr] + WIDTH,
                 self.cat['y'][self.curr] - WIDTH: self.cat['y'][self.curr] + WIDTH].T,
-                cmap='gray', origin='lower')
+                cmap='gray', norm=norm, origin='lower')
             self.before_imshow = axs2[1].imshow(
                 self.before_im.T[self.cat['x'][self.curr] - WIDTH: self.cat['x'][self.curr] + WIDTH,
                 self.cat['y'][self.curr] - WIDTH: self.cat['y'][self.curr] + WIDTH].T,
-                cmap='gray', origin='lower')
+                cmap='gray', norm=norm, origin='lower')
             self.after_imshow = axs2[2].imshow(
                 self.after_im.T[self.cat['x'][self.curr] - WIDTH: self.cat['x'][self.curr] + WIDTH,
                 self.cat['y'][self.curr] - WIDTH: self.cat['y'][self.curr] + WIDTH].T,
-                cmap='gray', origin='lower')
+                cmap='gray', norm=norm, origin='lower')
             self.load_text()
 
         with spectrum:
@@ -86,9 +94,9 @@ class SiftingWidget(widgets.HBox):
     def update(self, b):
         if b.description != 'skip':
             self.cat['class'][self.curr] = Classes[b.description.upper()]
-        if self.curr == len(self.cat) - 1:
-            self._init_next_scan()
         self.curr += 1
+        if self.curr == len(self.cat):
+            self._init_next_scan()
         self.load_mpl_im()
         self.fig2.canvas.draw()
         self.load_text()
@@ -101,27 +109,37 @@ class SiftingWidget(widgets.HBox):
         self.diff_im, self.header = fitsutils.read_image_fits(self.diff_ims[self.curr_scan])
         self.before_im, _ = fitsutils.read_image_fits(self.before_ims[self.curr_scan])
         self.after_im, _ = fitsutils.read_image_fits(self.after_ims[self.curr_scan])
+        rms = np.median(np.unique(self.cat['local_rms']))
+        self.big_diff_imshow.set_data(self.diff_im)
+        self.big_diff_imshow.set_norm(Normalize(vmin=-6 * rms, vmax=6 * rms))
+        self.fig1.canvas.draw()
         self.curr = 0
 
     def load_mpl_im(self):
+        peak = np.abs(self.cat['peak_flux'][self.curr])
+        norm = Normalize(vmin=-peak, vmax=peak)
         self.diff_imshow.set_data(
             self.diff_im.T[self.cat['x'][self.curr] - WIDTH: self.cat['x'][self.curr] + WIDTH,
             self.cat['y'][self.curr] - WIDTH: self.cat['y'][self.curr] + WIDTH].T)
+        self.diff_imshow.set_norm(norm)
         self.before_imshow.set_data(
             self.before_im.T[self.cat['x'][self.curr] - WIDTH: self.cat['x'][self.curr] + WIDTH,
             self.cat['y'][self.curr] - WIDTH: self.cat['y'][self.curr] + WIDTH].T)
+        self.before_imshow.set_norm(norm)
         self.after_imshow.set_data(
             self.after_im.T[self.cat['x'][self.curr] - WIDTH: self.cat['x'][self.curr] + WIDTH,
             self.cat['y'][self.curr] - WIDTH: self.cat['y'][self.curr] + WIDTH].T)
+        self.after_imshow.set_norm(norm)
 
     def load_text(self):
         coord = SkyCoord(ra=self.cat['ra'][self.curr] * u.deg, dec=self.cat['dec'][self.curr] * u.deg)
-        self.text.value = f"{self.curr + 1}/{len(self.cat)} candidates, {self.curr_scan + 1}/{len(self.catalogs)} scans" \
+        self.text.value = f"{self.cat.meta['DATE']} <br>" \
+                          f"{self.curr + 1}/{len(self.cat)} candidates, {self.curr_scan + 1}/{len(self.catalogs)} scans" \
                           f"<br> {coord.ra.to_string(u.hour)} {coord.dec.to_string()} " \
                           f"x={self.cat['x'][self.curr]}, y={self.cat['y'][self.curr]} " \
                           f"{self.cat['a'][self.curr] * 60:.2f}' x {self.cat['b'][self.curr] * 60:.2f}' " \
                           f"pk={self.cat['peak_flux'][self.curr]:.1f} Jy, " \
-                          f"noise={self.cat['local_rms'][self.curr]}:.2f Jy"
+                          f"noise={self.cat['local_rms'][self.curr]:.2f} Jy"
 
     def _load_catalog(self, cat_fits) -> Table:
         t = Table.read(cat_fits)
@@ -130,7 +148,7 @@ class SiftingWidget(widgets.HBox):
             # The split gets rid of the fractional second
             timestamp = datetime.strptime(t.meta['DATE'].split('.')[0], "%Y-%m-%dT%H:%M:%S")
             alt = coordutils.get_altaz_at_ovro(SkyCoord(t['ra'], t['dec'], unit=u.deg), timestamp).alt
-            t = t[alt > (self.min_alt_deg * u.deg)]
+            t = t[(alt > (self.min_alt_deg * u.deg)) & (t['peak_flux']/t['local_rms'] > SNR_CUTOFF)]
         t.add_column(Classes.NA.value, name='class')
         t.meta['COMMIT'] = gitutils.get_commit_id()
         return t
