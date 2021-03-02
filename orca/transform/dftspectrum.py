138431d648ff3782ba6407d46d4553276c5345a3
Yuping Huang <yupinghuang@users.noreply.github.com>
2021-03-02 13:50:51 -0800

    dftspectrum untested. WIP.

--- orca/transform/dftspectrum.py
+++ orca/transform/dftspectrum.py
@@ -35,10 +35,33 @@ def phase_shift_vis(t: table, freqs: np.array, phase_center: SkyCoord, pos: SkyC
     return data
 
 
-def phase_data_to_pos_inplace(data: np.array, uvw: np.array, freqs: np.array,
-                              ra0_rad: float, dec0_rad: float, ra_rad: float, dec_rad: float,
-                              weights: Optional[np.array] = None):
-    raise NotImplementedError
+def dftspectrum(data: np.array, uvw: np.array, freqs: np.array,
+                ra0_rad: float, dec0_rad: float, ra_rad: np.array, dec_rad: np.array,
+                weights: Optional[np.array] = None):
+    raise NotImplementedError('This is not working yet')
+    n_freqs = freqs.shape[-1]
+    n_uvws = uvw.shape[0]
+    n_src = ra_rad.shape[0]
+    assert uvw.shape[1] == 3
+    assert len(uvw.shape) == 2, 'uvw is assumed to be a 2-D array (n_uvw, 3)'
+    assert len(freqs.shape) <= 2, 'freqs should have leq 2 dimensions (single spw probably).'
+    assert data.shape[0] == n_uvws, 'Data is assumed to be ordered by N_UVWs and then N_FREQS'
+    assert data.shape[1] == n_freqs, 'Data is assumed to be ordered by N_UVWs and then N_FREQS'
+
+    freqs = freqs.astype(np.float32)
+    uvw = uvw.astype(np.float32)
+    lmn = np.empty(shape=(3, n_src), dtype=np.float32)
+
+    dra = ra_rad - ra0_rad
+    dec = dec_rad
+    dec0 = dec0_rad
+    lmn[0] = np.sin(dra) * np.cos(dec)
+    lmn[1] = np.sin(dec) * np.cos(dec0) - np.cos(dra) * np.cos(dec) * np.sin(dec0)
+    lmn[2] = np.sin(dec) * np.sin(dec0) + np.cos(dra) * np.cos(dec) * np.cos(dec0) - 1 # makes the work below easier
+    theta = -2 * np.pi * np.outer(uvw.dot(lmn), freqs) * INVERSE_C_MS
+    ph = np.cos(theta) + 1j * np.sin(theta)
+    return data.dot(ph[:, :, np.newaxis])
+
 
 
 def phase_data_to_pos_inplace(data: np.array, uvw: np.array, freqs: np.array,
