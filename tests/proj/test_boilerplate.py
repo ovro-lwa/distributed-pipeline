b87c5a3d43cda2063ce734756b175ca90280cdb6
Yuping Huang <yupinghuang@users.noreply.github.com>
2021-03-31 08:43:10 -0700

    Add a CASA sanity test.

--- tests/proj/test_boilerplate.py
+++ tests/proj/test_boilerplate.py
@@ -1,6 +1,13 @@
 import pytest
 
+from ..common import TEST_MS
+
 
 # Test import so that celery workers wouldn't fail silently on shutdown.
 def test_import():
     import orca.proj.boilerplate
+
+
+def test_casa_sanity():
+    from casatasks import listobs
+    listobs(TEST_MS)
