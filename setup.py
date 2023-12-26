666a79777ef96bff080b67872b3eec86228a9ca4
Yuping Huang <yupinghyper@gmail.com>
2023-12-26 21:28:00 +0000

    conda+pip, ready for removing pipenv

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name="ovro-lwa-orca", packages=find_packages(exclude=['tests', 'tests.*']),
-      # scripts specify those that can be executed from command-line.
-      scripts=['orca/flagging/flag_bls.py', 'orca/flagging/flag_bad_chans.py', 'orca/proj/start_workers.py', 'orca/extra/tempcal.py'],
-      version='0.1',
-      package_data={'orca': ['resources/*', 'default-orca-conf.yml']},
-      include_package_data=True
-      )
