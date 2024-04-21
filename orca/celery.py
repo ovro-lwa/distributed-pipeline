2548fc5f422679d570aee74cc8cf2605423a8655
Yuping Huang <yupinghyper@gmail.com>
2024-04-21 20:41:03 +0000

    get rid of result expiration?

--- orca/celery.py
+++ orca/celery.py
@@ -6,13 +6,12 @@ CELERY_APP_NAME = 'orca'
 
 # TODO: what if I import a module that imports orca.transform.*?
 app = Celery(CELERY_APP_NAME,
-             broker='pyamqp://pipe:pipe@lwacalimhead:5672/pipe',
+             broker='pyamqp://pipe:pipe@rabbitmq.calim.mcs.pvt:5672/pipe',
              backend='redis://10.41.0.85:6379/0',
              include=['orca.transform.calibration'])
 
 # Optional configuration, see the application user guide.
 app.conf.update(
-    result_expires=3600,
     worker_prefetch_multiplier=1,
     task_serializer='json',
     broker_connection_retry=True,
