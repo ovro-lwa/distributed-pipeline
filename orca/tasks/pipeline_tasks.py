4267387baa2e7d9fdc5be4c1ab6dd224c909fdcc
nkosogor <nakosogorov@gmail.com>
2024-12-03 21:00:38 +0000

    Add averaging and changing phase center into the pipeline

--- orca/tasks/pipeline_tasks.py
+++ orca/tasks/pipeline_tasks.py
@@ -6,7 +6,8 @@ from orca.transform.flagging import flag_with_aoflagger as original_flag_with_ao
 from orca.transform.calibration import applycal_data_col as original_applycal_data_col
 from orca.wrapper.wsclean import wsclean as original_wsclean
 from orca.wrapper.ttcal import peel_with_ttcal 
-
+from orca.transform.averagems import average_frequency
+from orca.wrapper import change_phase_centre
 from typing import List
 
 @app.task
@@ -19,6 +20,9 @@ def flag_with_aoflagger_task(ms: str, strategy: str, in_memory: bool, n_threads:
 
 @app.task
 def applycal_data_col_task(ms: str, gaintable: str) -> str:
+    """
+    Celery task to apply calibration to an MS.
+    """
 #    return original_applycal_data_col(ms, gaintable)
     out_ms = ms.rstrip('/') + '_calibrated.ms'
     return original_applycal_data_col(ms, gaintable, out_ms)
@@ -26,7 +30,9 @@ def applycal_data_col_task(ms: str, gaintable: str) -> str:
 @app.task
 def wsclean_task(ms: str, out_dir: str, filename_prefix: str, extra_args: List[str],
                  num_threads: int, mem_gb: int) -> None:
-    return original_wsclean([ms], out_dir, filename_prefix, extra_args, num_threads, mem_gb)
+    original_wsclean([ms], out_dir, filename_prefix, extra_args, num_threads, mem_gb)
+    return ms
+    #return original_wsclean([ms], out_dir, filename_prefix, extra_args, num_threads, mem_gb)
 
 @app.task
 def peel_with_ttcal_task(ms: str, sources: str) -> str:
@@ -34,3 +40,25 @@ def peel_with_ttcal_task(ms: str, sources: str) -> str:
     Celery task to use TTCal to peel sources.
     """
     return peel_with_ttcal(ms, sources)
+
+@app.task
+def average_frequency_task(ms: str, chanbin: int = 4) -> str:
+    """
+    Celery task to perform frequency averaging on an MS.
+    """
+    output_vis = ms.rstrip('/') + '_averaged.ms'
+    return average_frequency(vis=ms, output_vis=output_vis, chanbin=chanbin)
+
+@app.task
+def change_phase_center_task(ms: str, new_phase_center: str) -> str:
+    """
+    Celery task to change the phase center of a calibrated and averaged MS.
+    """
+    try:
+        # Execute the phase center change
+        updated_ms = change_phase_centre.change_phase_center(ms, new_phase_center)
+        return updated_ms
+    except Exception as e:
+        raise RuntimeError(f"Failed to change phase center for {ms}: {e}")
+
+
